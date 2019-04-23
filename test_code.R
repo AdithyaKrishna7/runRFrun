35cbd0d805d9f8971055a924726fa605af948b0a
Adithya <adithyakrishna@student.uts.edu.au>
2019-04-23 14:50:49 +1000

    Test commit

---
+++ test_code.R
@@ -0,0 +1,11 @@
+library(dplyr)
+library(sqldf)
+library(ggplot2)
+
+base_data <- readRDS("D:/UTS/MDSI/Sem 01/Data, Algos, and Meaning/Assignments/DAM Assignment 2/student_data/student_data/AT2_train_STUDENT.rds")
+
+names(base_data)
+
+str(base_data)
+
+glimpse(base_data)
